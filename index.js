e18a83e2879eab269b3bdaaf372157cfa0349d56
sunny-g <sunny.gonna@gmail.com>
2016-02-12 16:38:34 -0800

    video 4

--- index.js
+++ index.js
@@ -7,25 +7,46 @@
 
 // logic
 function main() {
-  return Rx.Observable.timer(0, 1000)
-    .map((i) => `seconds elapsed ${i}`)
+  // allows us to provide different sinks to different effects
+  return {
+    DOM: Rx.Observable.timer(0, 1000)
+      .map(i => `seconds elapsed ${i}`),
+
+    Log: Rx.Observable.timer(0, 2000)
+      .map(i => i * 2)
+  }
 }
 
 // effects
-function DOMEffect(text$) {
+function DOMDriver(text$) {
   return text$.subscribe(text => {
     const container = document.querySelector('#app');
     container.textContent = text;
   });
 }
 
-function consoleEffect(msg$) {
+function consoleDriver(msg$) {
   return msg$.subscribe(msg => console.log(msg));
 }
 
 // hooks up the logic to the effects
 
 // like a kitchen sink where "water comes into the sink and is drained to some effect" 
-const sink = main();
-DOMEffect(sink);
-consoleEffect(sink);
+function run(main, drivers) {
+  const sinks = main();
+  Object.keys(drivers).forEach((driver) => {
+    if (sinks[driver]) drivers[driver](sinks[driver]);
+  });
+  return {sinks, sources: drivers}
+}
+
+/*
+  drivers are an interface between software (programs) and hardware
+  drivers connect the program (logic) to the hardware (effects)
+ */
+const drivers = {
+  DOM: DOMDriver,
+  Log: consoleDriver
+}
+
+run(main, drivers)
